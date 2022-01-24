fb39f5cbf9cb5191202fbd5b0369f677fb0240b5
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-24 09:09:26 +0200

    first upload

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <Navbar/>
+ <Intro/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Intro from './components/Intro.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Navbar,
+   Intro
   }
 }
 </script>
