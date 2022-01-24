b1a2734fa08908591f5db3c561e83ab3afab0732
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-24 10:19:17 +0200

    added footer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
  <Navbar/>
  <Intro/>
 <About/>
+<Experience/>
+<Skills/>
+<Projects/>
+<Testimonials/>
+<Contact/>
+<Footer/>
+
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
 import Intro from './components/Intro.vue';
-import About from './components/About.vue'
+import About from './components/About.vue';
+import Contact from './components/Contact.vue';
+import Footer from './components/Footer.vue';
+import Projects from './components/Projects.vue';
+import Skills from './components/Skills.vue';
+import Testimonials from './components/Testimonials.vue';
+import Experience from './components/Experience.vue';
+
 
 
 export default {
@@ -15,7 +29,13 @@ export default {
   components: {
    Navbar,
    Intro,
-   About
+   About,
+   Contact,
+   Footer,
+   Projects,
+   Skills,
+   Testimonials,
+   Experience
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+<footer>
+  <p>Developed by TDL2627</p>
+  </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+footer{
+    background-color: black;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+    
+}
+p{
+    color: white;
+}
+</style>
