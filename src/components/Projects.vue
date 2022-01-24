02dab38175954fe935bc6e44ecc140ce64cd455b
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-24 14:25:00 +0200

    added carousel

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,278 @@
 <template>
 <div id="four">
   <h1 class="heading">Projects</h1>
+   <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner"
+          style="border: 20px solid black ; border-radius: 25px; ">
+          <div class="carousel-item active">
+            <img src="/images/project/quiz.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">QUIZ APP</h1>
+            <p class="p-details">This is a riddle game made with PHP,HTML and CSS.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/quiz">GITHUB LINK</a>
+            <a target="_blank" class="web-link buit" href="https://tdl2627-quiz.herokuapp.com/">HEROKU LINK</a>
+            <br><br>
+
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/blog.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">BLOG APP</h1>
+            <p class="p-details">This is a colabrative full MEAN stack blog application that was made with
+              Angular,HTML,BOOTSTRAP,CSS,NODE JS ,FIRE BASE and JavaScript.
+              Here you can login and share your coding experience.
+            </p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <br>
+            <br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/blog-app">GITHUB LINK</a> <a
+              target="_blank" class="web-link buit" href="https://tech-path.herokuapp.com/landing-page">HEROKU LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/reactor.png" class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Reaction Game</h1>
+            <p class="p-details">This game checks your reaction speed.It is created with Vue
+            </p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <br>
+            <br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/ReactGame">GITHUB LINK</a> <a
+              target="_blank" class="web-link buit" href="https://reactor2627.web.app/">HEROKU LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/calculator.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">CALCULATOR</h1>
+            <p class="p-details">A simple calculator using JavaScript,HTML and CSS.
+            </p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+            <br>
+            <br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/calculator">GITHUB LINK</a> <a
+              target="_blank" class="web-link buit" href="https://tdl2627-calculator.netlify.app/">NETLIFY LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/anime.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">ANIMATION</h1>
+            <p class="p-details">This a basic animation made with CSS and HTML.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/basic-anime">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-anime.netlify.app/">NETLIFY LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/form.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">FORM</h1>
+            <p class="p-details">This is a functional basic form using formspree,HTML and CSS.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/a-basic-form">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-form.netlify.app/">NETLIFY LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/interior.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">BUSINESS SITE</h1>
+            <p class="p-details">One of my first websites that consists of HTML and CSS.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/interior">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://interior-tdl2627.netlify.app/">NETLIFY LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/card.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">CARDS</h1>
+            <p class="p-details">A basic layout of cards using HTML and CSS.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/basic-cards">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-cards.netlify.app/">NETLIFY LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/doob.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">SINGLE PAGE SITE</h1>
+            <p class="p-details">A mock of single page using HTML and CSS.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/single-page">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-single-page.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/single.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">MOCK SITE</h1>
+            <p class="p-details">Another mock of single page using BOOTSTRAP,HTML and CSS.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/doob-single-page">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-a-single-page.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/guns.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Simple CRUD</h1>
+            <p class="p-details">Uses javascript to create a demo ecommerce site.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/advanceCrud">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://guns-2627.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/pokedex.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">POKEDEX</h1>
+            <p class="p-details">A javascript site using fetch api.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/pokedex">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-pokedex.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/tempConvert.png" class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Temperature Converter</h1>
+            <p class="p-details">Using JavaScript.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/temperature-converter">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-temp-convert.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/swegga.png" class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Swegga</h1>
+            <p class="p-details">Using JavaScript for calculations.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/swegga">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-swegga.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/omni.png"  class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Omni Bar</h1>
+            <p class="p-details">A unique super nav bar.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked "></span>
+            <span class="fa fa-star checked "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/omniBar.git">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://omnibar.netlify.app">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/clock.png" class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Clock</h1>
+            <p class="p-details">A clock that greets according to the time.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star  "></span>
+            <span class="fa fa-star  "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/clock">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-clock.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+          <div class="carousel-item">
+            <img src="/images/project/charizard.png" class="d-block w-100" alt="...">
+            <h1 class="subbed-head1">Charizard Fan Page</h1>
+            <p class="p-details">A simple fan page created using API.</p>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked "></span>
+            <span class="fa fa-star "></span>
+            <br><br>
+            <a target="_blank" class="web-link buit" href="https://github.com/TDL2627/charizard">GITHUB LINK</a><a
+              target="_blank" class="web-link buit" href="https://tdl2627-charizard.netlify.app/">NETLIFY
+              LINK</a>
+            <br><br>
+          </div>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span aria-hidden="true"><img class="carousel-button buit" 
+              src="https://img.icons8.com/material-outlined/50/000000/left.png" /></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span aria-hidden="true"><img class="carousel-button buit"
+              src="https://img.icons8.com/material-outlined/50/000000/right.png" /></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
 </div>
 
 </template>
@@ -12,9 +284,12 @@ export default {
 </script>
 
 <style scoped>
-div{
+#four{
   background-color: grey;
   height: 90vh;
+}
+.carousel-inner{
+  height: 500px ;
 }
 h1{
   color: white;
--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <style scoped>
-div{
+#five{
   background-color: black;
   height: 90vh;
 }
