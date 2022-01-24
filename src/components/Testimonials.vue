139f2bfae0c48d5e8e86b31f461ebaebe00ca8f2
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-24 14:44:08 +0200

    added accordions

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,6 +1,125 @@
 <template>
 <div id="five">
  <h1 class="heading">Testimonials</h1>  
+  <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="accordion-item testi" data-aos="fade-left">
+              <img class="people-pic" src="/images/people/Godwin.jpeg" alt="godwin">
+              <h2 class="accordion-header" id="headingOne">
+
+                <button class="accordion-button collapsed buit" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  Godwin
+                </button>
+              </h2>
+              <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <h3 class="testi-title">Head of Curriculum and Learning</h3> "Based on the time that I have known Ashley, he will measure
+                  up to high organisational expectations."
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="accordion-item testi" data-aos="fade-right">
+              <img class="people-pic" src="/images/people/jason-wandrag-nobg-2.png" alt="jason">
+              <h2 class="accordion-header" id="heading3">
+
+                <button class="accordion-button collapsed buit" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+                  Jason
+                </button>
+              </h2>
+              <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading3"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <h3 class="testi-title">Lecturer</h3> "Ashley brings a unique energy to my class. He enjoys participating in class
+                  activities. It's always great to see him well-behaved in my class. I am excited about Ashley's
+                  potential and I can't wait to see how he will grow in the future."
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="accordion-item testi" data-aos="fade-left">
+              <img class="people-pic" src="/images/people/Bulela1.jpg" alt="bulela">
+              <h2 class="accordion-header" id="heading4">
+
+                <button class="accordion-button collapsed buit" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapse4">
+                  Bulela
+                </button>
+              </h2>
+              <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading4"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <h3 class="testi-title">Peer</h3> "He can work well with others."
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="accordion-item testi" data-aos="fade-right">
+              <img class="people-pic" src="/images/people/Jude3.jpg" alt="jude">
+              <h2 class="accordion-header" id="heading5">
+
+                <button class="accordion-button collapsed buit" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+                  Jude
+                </button>
+              </h2>
+              <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading5"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <h3 class="testi-title">Peer</h3> "He knows what he is doing."
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="accordion-item testi" data-aos="fade-left">
+              <img class="people-pic" src="/images/people/Kischa4.jpg" alt="Kischa">
+              <h2 class="accordion-header" id="heading6">
+
+                <button class="accordion-button collapsed buit" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapse6">
+                  Kischa
+                </button>
+              </h2>
+              <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="heading6"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <h3 class="testi-title">Peer</h3> "He is very creative."
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="accordion-item testi" data-aos="fade-right">
+              <img class="people-pic" src="/images/people/Seth3.jpg" alt="seth">
+              <h2 class="accordion-header" id="heading8">
+
+
+                <button class="accordion-button collapsed buit" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#collapse8" aria-expanded="false" aria-controls="collapse8">
+                  Seth
+                </button>
+              </h2>
+              <div id="collapse8" class="accordion-collapse collapse" aria-labelledby="heading8"
+                data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                  <h3 class="testi-title">Peer</h3> "He is always willing to help."
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
   </div> 
 </template>
 
