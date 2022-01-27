200e4758d42e1fb29d6694fbf2538f479fce9aac
TDL2627 <the.dark.lord.2627@gmail.com>
2022-01-27 09:19:52 +0200

    finished projects for now

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,7 +5,7 @@
         <div class="carousel-inner"
           style="border: 20px solid black ; border-radius: 25px; ">
           <div class="carousel-item active">
-            <img src="../assets/project/quiz.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/quiz.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">QUIZ APP</h1>
             <p class="p-details">This is a riddle game made with PHP,HTML and CSS.</p>
             <span class="fa fa-star checked"></span>
@@ -20,7 +20,7 @@
 
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/blog.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/blog.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">BLOG APP</h1>
             <p class="p-details">This is a colabrative full MEAN stack blog application that was made with
               Angular,HTML,BOOTSTRAP,CSS,NODE JS ,FIRE BASE and JavaScript.
@@ -38,7 +38,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/reactor.png" class="d-block w-100" alt="...">
+            <img src="../assets/project/reactor.png" class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Reaction Game</h1>
             <p class="p-details">This game checks your reaction speed.It is created with Vue
             </p>
@@ -54,7 +54,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/calculator.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/calculator.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">CALCULATOR</h1>
             <p class="p-details">A simple calculator using JavaScript,HTML and CSS.
             </p>
@@ -70,7 +70,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/anime.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/anime.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">ANIMATION</h1>
             <p class="p-details">This a basic animation made with CSS and HTML.</p>
             <span class="fa fa-star checked"></span>
@@ -84,7 +84,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/form.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/form.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">FORM</h1>
             <p class="p-details">This is a functional basic form using formspree,HTML and CSS.</p>
             <span class="fa fa-star checked"></span>
@@ -98,7 +98,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/interior.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/interior.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">BUSINESS SITE</h1>
             <p class="p-details">One of my first websites that consists of HTML and CSS.</p>
             <span class="fa fa-star checked"></span>
@@ -112,7 +112,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/card.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/card.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">CARDS</h1>
             <p class="p-details">A basic layout of cards using HTML and CSS.</p>
             <span class="fa fa-star checked"></span>
@@ -126,7 +126,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/doob.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/doob.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">SINGLE PAGE SITE</h1>
             <p class="p-details">A mock of single page using HTML and CSS.</p>
             <span class="fa fa-star checked"></span>
@@ -141,7 +141,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/single.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/single.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">MOCK SITE</h1>
             <p class="p-details">Another mock of single page using BOOTSTRAP,HTML and CSS.</p>
             <span class="fa fa-star checked"></span>
@@ -156,7 +156,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/guns.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/guns.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Simple CRUD</h1>
             <p class="p-details">Uses javascript to create a demo ecommerce site.</p>
             <span class="fa fa-star checked"></span>
@@ -171,7 +171,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/pokedex.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/pokedex.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">POKEDEX</h1>
             <p class="p-details">A javascript site using fetch api.</p>
             <span class="fa fa-star checked"></span>
@@ -186,7 +186,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/tempConvert.png" class="d-block w-100" alt="...">
+            <img src="../assets/project/tempConvert.png" class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Temperature Converter</h1>
             <p class="p-details">Using JavaScript.</p>
             <span class="fa fa-star checked"></span>
@@ -201,7 +201,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/swegga.png" class="d-block w-100" alt="...">
+            <img src="../assets/project/swegga.png" class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Swegga</h1>
             <p class="p-details">Using JavaScript for calculations.</p>
             <span class="fa fa-star checked"></span>
@@ -216,7 +216,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/omni.png"  class="d-block w-100" alt="...">
+            <img src="../assets/project/omni.png"  class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Omni Bar</h1>
             <p class="p-details">A unique super nav bar.</p>
             <span class="fa fa-star checked"></span>
@@ -231,7 +231,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/clock.png" class="d-block w-100" alt="...">
+            <img src="../assets/project/clock.png" class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Clock</h1>
             <p class="p-details">A clock that greets according to the time.</p>
             <span class="fa fa-star checked"></span>
@@ -246,7 +246,7 @@
             <br><br>
           </div>
           <div class="carousel-item">
-            <img src="../assets/project/charizard.png" class="d-block w-100" alt="...">
+            <img src="../assets/project/charizard.png" class="d-block w-100 pro" alt="...">
             <h1 class="subbed-head1">Charizard Fan Page</h1>
             <p class="p-details">A simple fan page created using API.</p>
             <span class="fa fa-star checked"></span>
@@ -263,12 +263,12 @@
 
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-          <span aria-hidden="true"><img class="carousel-button buit" 
+          <span aria-hidden="true"><img class="carousel-button buit1" 
               src="https://img.icons8.com/material-outlined/50/000000/left.png" /></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-          <span aria-hidden="true"><img class="carousel-button buit"
+          <span aria-hidden="true"><img class="carousel-button buit1"
               src="https://img.icons8.com/material-outlined/50/000000/right.png" /></span>
           <span class="visually-hidden">Next</span>
         </button>
@@ -291,11 +291,44 @@ export default {
 .carousel-inner{
   height: 500px ;
 }
-h1{
+h1,p{
   color: white;
 }
-img{
+.pro{
   height: 250px;
+  margin-left: 450px;
+  margin-top: 10px;
   width: 350px !important;
+}
+.carousel{
+  margin: 10px;
+}
+.buit{
+  background-color: black;
+  color: white;
+ border-radius: 25px;
+ padding: 10px;
+ margin: 10px;
+ border: none;
+ text-decoration: none;
+}
+.buit:hover{
+  background-color: red;
+  color: white;
+ border-radius: 25px;
+}
+.buit1{
+  background-color: white;
+  color: white;
+ border-radius: 25px;
+ padding: 10px;
+ margin: 10px;
+ border: none;
+ text-decoration: none;
+}
+.buit1:hover{
+  background-color: red;
+  color: white;
+ border-radius: 25px;
 }
 </style>
